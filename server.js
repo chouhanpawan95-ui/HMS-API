db148f184a26c8809f7774b254d2e19772146104
chouhanpawan95-ui <chouhanpawan95@gmail.com>
2025-11-06 11:03:05 +0530

    Fix case-sensitive filename for user model

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
-// server.js
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const cors = require('cors');
 
 const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'https://hms-front-0mb7.onrender.com/',
+  origin: 'https://hms-front-0mb7.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
-
 app.use(express.json({ limit: '5mb' }));
 
 // MongoDB Connection
@@ -44,10 +40,10 @@ app.use('/api/patients', patientRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/engineer-visits', engineerVisitRoutes);
 
-// Basic Auth Model
+// Model Import
 const User = require('./models/user');
 
-// Error Handler Middleware
+// Error Handler
 app.use((err, req, res, next) => {
   console.error('Unhandled Error:', err);
   res.status(500).json({ message: 'Internal server error' });
@@ -55,8 +51,6 @@ app.use((err, req, res, next) => {
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-
-// Connect to DB and Start Server
 (async () => {
   try {
     await connectDB();
