3622c868583878f924348f9004c2f34288d17df1
chouhanpawan95-ui <chouhanpawan95@gmail.com>
2026-01-21 16:21:45 +0530

    token change

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-//const User = require('../models/User');
+const User = require('../models/user');
 const router = express.Router();
 const verifyToken = require('../middleware/authMiddleware')
 // ✅ Sign Up
@@ -34,7 +34,7 @@ router.post('/login', async (req, res) => {
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '2h' });
     res.status(200).json({ message: 'Login successful', token });
   } catch (error) {
-    res.status(500).json({ message: 'Server error', error });
+    res.status(500).json({ message: 'Server error', error: error.message });
   }
 });
 
