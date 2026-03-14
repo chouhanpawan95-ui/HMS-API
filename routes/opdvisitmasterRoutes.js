f47da1a0a9e038a6be6fda660aeb21ae6ed1c688
chouhanpawan95-ui <chouhanpawan95@gmail.com>
2026-03-14 20:28:54 +0530

    manage route

--- routes/opdvisitmasterRoutes.js
+++ routes/opdvisitmasterRoutes.js
@@ -2,12 +2,27 @@
 const express = require('express');
 const router = express.Router();
 const opdController = require('../controllers/opdvisitmasterController');
-const verifyToken = require('../middleware/authMiddleware');
 
-router.post('/', verifyToken, opdController.createOpdVisit); // create
+router.post('/', opdController.createOpdVisit); // create
 router.get('/',opdController.getOpdVisits); // list
-router.get('/:id', verifyToken, opdController.getOpdVisitById); // get by id or pkVisitId
-router.put('/:id', verifyToken, opdController.updateOpdVisit); // update
-router.delete('/:id', verifyToken, opdController.deleteOpdVisit); // delete
+router.get('/:id', opdController.getOpdVisitById); // get by id or pkVisitId
+router.put('/:id', opdController.updateOpdVisit); // update
+router.delete('/:id', opdController.deleteOpdVisit); // delete
 
 module.exports = router;
+
+
+///
+// // routes/opdvisitmasterRoutes.js
+// const express = require('express');
+// const router = express.Router();
+// const opdController = require('../controllers/opdvisitmasterController');
+// const verifyToken = require('../middleware/authMiddleware');
+
+// router.post('/', verifyToken, opdController.createOpdVisit); // create
+// router.get('/', verifyToken, opdController.getOpdVisits); // list
+// router.get('/:id', verifyToken, opdController.getOpdVisitById); // get by id or pkVisitId
+// router.put('/:id', verifyToken, opdController.updateOpdVisit); // update
+// router.delete('/:id', verifyToken, opdController.deleteOpdVisit); // delete
+
+// module.exports = router;
