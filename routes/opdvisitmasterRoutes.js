47fbcede2ec5a1065e04a9fdd0bd981498724a30
chouhanpawan95-ui <chouhanpawan95@gmail.com>
2026-03-17 18:25:20 +0530

    manage route

--- routes/opdvisitmasterRoutes.js
+++ routes/opdvisitmasterRoutes.js
@@ -1,28 +1,28 @@
 // routes/opdvisitmasterRoutes.js
-const express = require('express');
-const router = express.Router();
-const opdController = require('../controllers/opdvisitmasterController');
+// const express = require('express');
+// const router = express.Router();
+// const opdController = require('../controllers/opdvisitmasterController');
 
-router.post('/', opdController.createOpdVisit); // create
-router.get('/',opdController.getOpdVisits); // list
-router.get('/:id', opdController.getOpdVisitById); // get by id or pkVisitId
-router.put('/:id', opdController.updateOpdVisit); // update
-router.delete('/:id', opdController.deleteOpdVisit); // delete
+// router.post('/', opdController.createOpdVisit); // create
+// router.get('/',opdController.getOpdVisits); // list
+// router.get('/:id', opdController.getOpdVisitById); // get by id or pkVisitId
+// router.put('/:id', opdController.updateOpdVisit); // update
+// router.delete('/:id', opdController.deleteOpdVisit); // delete
 
-module.exports = router;
+// module.exports = router;
 
 
 ///
 // // routes/opdvisitmasterRoutes.js
-// const express = require('express');
-// const router = express.Router();
-// const opdController = require('../controllers/opdvisitmasterController');
-// const verifyToken = require('../middleware/authMiddleware');
+const express = require('express');
+const router = express.Router();
+const opdController = require('../controllers/opdvisitmasterController');
+const verifyToken = require('../middleware/authMiddleware');
 
-// router.post('/', verifyToken, opdController.createOpdVisit); // create
-// router.get('/', verifyToken, opdController.getOpdVisits); // list
-// router.get('/:id', verifyToken, opdController.getOpdVisitById); // get by id or pkVisitId
-// router.put('/:id', verifyToken, opdController.updateOpdVisit); // update
-// router.delete('/:id', verifyToken, opdController.deleteOpdVisit); // delete
+router.post('/', verifyToken, opdController.createOpdVisit); // create
+router.get('/', verifyToken, opdController.getOpdVisits); // list
+router.get('/:id', verifyToken, opdController.getOpdVisitById); // get by id or pkVisitId
+router.put('/:id', verifyToken, opdController.updateOpdVisit); // update
+router.delete('/:id', verifyToken, opdController.deleteOpdVisit); // delete
 
-// module.exports = router;
+module.exports = router;
